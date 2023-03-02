d56c0f2b8a53ddc2a846f6277fcfe1d26de5c563
hassan-ghanem <31302663+hassan-ghanem@users.noreply.github.com>
2023-03-02 18:49:35 +0300

    correct class name for setClassForTemplateLoading

--- email-worker-example/src/main/java/org/example/configuration/FreemarkerConfig.java
+++ email-worker-example/src/main/java/org/example/configuration/FreemarkerConfig.java
@@ -1,6 +1,5 @@
 package org.example.configuration;
 
-import org.example.email.EmailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +16,7 @@ public class FreemarkerConfig {
 		
 		cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_31);
 		cfg.setDefaultEncoding(ENCODING);
-		cfg.setClassForTemplateLoading(EmailService.class, TEMPLATES_PATH);
+		cfg.setClassForTemplateLoading(FreemarkerConfig.class, TEMPLATES_PATH);
 		
 		return cfg;
 	}
